00ec81ba63a4d433e1464277700a4161aa1f9b61
Mohit Kumar Dey <mohitdey0709@gmail.com>
2025-12-08 23:53:40 +0000

    Learned about JS Variables

---
+++ Basics_01/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 1445 // const is used for making variable immutable i.e, it cannot be changed.
+let accountEmail = "deymohit5@gmail.com"
+var accountPassword = "12345"
+
+accountCity = "Guwahati"
+
+// accountId = 2 (not allowed)
+
+accountEmail = "deymohit02@gmail.com"
+accountPassword = "1223"
+accountCity = "Manali"
+
+console.log(accountId);
+
+console.table([accountEmail, accountPassword, accountCity])
