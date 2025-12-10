a36a810e3f472c7de6dc5af73cda638f4441784e
Mohit Kumar Dey <mohitdey0709@gmail.com>
2025-12-10 22:17:56 +0000

    Learnt about different DT, string, int, bigint, boolean, etc

--- Basics_01/01_dataTypes.js
+++ Basics_01/01_dataTypes.js
@@ -1,3 +1,23 @@
 "use strict"; // treats all the JS code as newer version
 
-alert("hello") //we are using node.js not browser so it will not run
+//alert("hello") //we are using node.js not browser so it will not run
+
+let name = "Mohit"
+let age = 23
+let isLoggedIn = false
+
+// Types of DataTypes
+// boolean => True/False
+// number => 0 to 2^53
+// bigint
+// string => ""
+// null => standalone value
+// undefined => Variable declared but not assigned
+// symbol => Used mainly in React to differentiate individual unique components
+
+// object => 
+
+console.log(typeof "Mohit")
+console.log(typeof undefined);
+console.log(typeof null);
+
